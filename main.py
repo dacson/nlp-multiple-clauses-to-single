70c5f0bb50710efae0104b1862c6acd34d29599a
Henri Lefebvre <henri.pasdecalais@yahoo.fr>
2019-01-23 00:00:59 +0100

    add coordination for np and vp

--- main.py
+++ main.py
@@ -4,36 +4,39 @@ from queue import Queue
 grammar = """
     start: sentence"."
     sentence: np vp
-        | sentence "and" sentence
-        | sentence "," sentence
     np: (DET  | ) (CARD | ) (ORD | ) (QUANT | ) (ap | ) NOUN
+        | np COORD np
     ap: ADJ -> concat | ADJ ap -> concat
-    vp: conjugated_verb
+    vp: conjugated_verb (np | ) -> concat
+        | vp COORD vp
     
     conjugated_verb: (AUXILIARY | ) (NOT | ) VERB -> concat
         | VERB"s" -> thrd_prsn_conjuagated_verb
         | (BE | ) (NOT | ) (VERB_ING | VERB_EN | ) -> concat
         | HAVE (NOT | ) (VERB_EN | ) -> concat
 
+    COORD.2: "&"
+        | "nor"
+        | "or"
+        | "and"
     DET: "these" | "the" | "an" | "a" | "this" | "those" | "that"
     CARD: "first" | "second"
     ORD: "two" | "three" | "four"
     QUANT: "few"
-    NOUN: "dog" | "I"
+    NOUN: "dog" | "I" | "cat" | "man"
 
     ADJ: "big" | "sad" | "small" | "red" | "happy"
 
     BE: "is" | "are" | "have been" | "has been"
     HAVE: "have" | "has"
-    VERB: "eat" | "smile" | "love"
+    VERB: "eat" | "smile" | "love" | "kill"
     VERB_ING: VERB"ing" | "smiling"
     VERB_ED: VERB"ed" | "knew"
     VERB_EN: VERB"ed" | "known"
     AUXILIARY: "do" | "does" | "did" | "will"
     NOT: "not" | "n't"
 
-    %import common.WS_INLINE
-    %ignore WS_INLINE
+    %ignore " "
 """
 
 @v_args(inline=True)    # Affects the signatures of the methods
@@ -50,8 +53,8 @@ class MultipleClauseTransformer(Transformer):
     def thrd_prsn_conjuagated_verb(self, verb): return verb + "s";
     def ing_conjugated_verb(self, be, verb): return be + " " + verb + "ing";
 
-nlp_parser = Lark(grammar, parser='lalr', transformer=MultipleClauseTransformer())
+nlp_parser = Lark(grammar, parser='lalr', transformer=MultipleClauseTransformer());
 
-result = nlp_parser.parse("a big sad dog did not eat.");
+result = nlp_parser.parse("a big sad dog did not eat or killed the cat nor the man.");
 
 print(result);
